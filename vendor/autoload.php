02c393fb990d1b9173f73b891b8d31ea65bde26a
bobdenotter <bob@twokings.nl>
2014-07-31 16:33:52 +0200

    Updatig to bolt 1.6.10

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit99bc955fd0a2ddb4abbb3bef04fcf680::getLoader();
+return ComposerAutoloaderInitb71201fbb420d4dd76d39d4d3349bc42::getLoader();
